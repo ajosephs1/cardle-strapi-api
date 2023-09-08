d6cb0591c6ee1d59249864bcd46abce0ee9d7109
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-09-08 14:14:05 -0400

    Add CORS access

--- config/middlewares.js
+++ config/middlewares.js
@@ -20,7 +20,18 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: 'http://localhost:3000', 
+      expose: ['WWW-Authenticate', 'Server-Authorization'],
+      maxAge: 31536000,
+      credentials: true,
+      methods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD',
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
